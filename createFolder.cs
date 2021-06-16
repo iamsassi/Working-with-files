90f8e3a938a75c9621ed74e737aded6c6c48df76
iamsassi <marcus.sassi@gmail.com>
2021-06-16 18:56:17 +0200

    Add project files.

---
+++ createFolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Working_with_files
+{
+    class createFolder
+    {
+        public string dirName;
+
+        public createFolder(string dirName)
+        {
+            if(Directory.Exists(dirName))
+                {
+                Console.WriteLine($"The folder '{dirName}' exists already");
+            }
+            else
+            {
+                Directory.CreateDirectory(dirName);
+                Console.WriteLine($"The folder '{dirName}' has been created");
+            }
+        }
+    }
+}
